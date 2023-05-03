8a04001f0a6625c23994d2f9f9b4b0ebd45e3ca0
Leonardo Zambrana <leonardozambrana1@gmail.com>
2023-05-03 13:59:21 -0300

    agora sim terminei

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
-let btn = document.getElementById('btn');
 let mountains_front = document.getElementById('mountains_front');
 let header = document.querySelector('header');
 
@@ -28,9 +27,7 @@ window.addEventListener('scroll', function(){
   mountains_front.style.top = value * 0 + 'px'
   text.style.marginRight = value * 1.5 + 'px'
   text.style.marginTop = value * 1.5 + 'px'
-  btn.style.marginTop = value * 1.5 + 'px'
   header.style.top = value * 0.5 + 'px'
-
 })
 
 botaoSobre.addEventListener('click', ()=>{
